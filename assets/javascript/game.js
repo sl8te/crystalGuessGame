f0e007bf563d9af390fd830d978971bf7e04e9a4
Slayte Eaton <slayteeaton@slaytes-mbp.attlocal.net>
2018-11-02 18:04:30 -0500

    Added pseudocode to the JavaScript file

---
+++ assets/javascript/game.js
@@ -0,0 +1,39 @@
+//Restart Game
+    //let user score = 0
+    //let Computer randomly assign new game score.
+    //let Computer randomly assign new point values to each crystal. 
+        //continue to keep hidden from user
+
+//Game Start:
+    //Computer randomly assigns a new game score.
+    //User score is resets to zero. 
+    //Computer randomly assigns new point values to each crystal.
+
+//Game Play: 
+    //User click's a crystal. 
+        //this crystal's point value is added to the user's score.
+    // wait for user's next click to repeat function again.
+            
+      
+       //As long as the user's score < game score, 
+            //user can continue game play.
+        //if the user's score = game score 
+            //trigger the game won response function.
+        //else if the user's score > game score 
+            //trigger the game lost response function.
+
+//Game Over:
+    //Game Won response.
+        //add 1 point to the games won category.
+        //restart game
+    //Game Lost response.
+        //add -1 point to games lost.
+        //restart game
+
+//On window load:
+    //let game won = 0 
+    //let game lost = 0
+    //let user score = 0
+    //let Computer randomly assign game score.
+    //let Computer randomly assign new point values to each crystal. 
+        //keep hidden from user     
